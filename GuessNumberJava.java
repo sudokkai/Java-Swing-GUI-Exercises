d470013fe21545763c7b293d654b3ea370fd36e3
sudokkai <kai.glaisek@student.tdsb.on.ca>
2024-04-17 11:15:59 -0400

    Exercise 2

---
+++ GuessNumberJava.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+public class GuessNumberJava {
+    public static void main(String[] args) throws Exception {
+        boolean start = true; //boolean for while loop
+        double num = Math.round(Math.random()*100); //creating a random number between 100 and 0
+        while (start){ //while loop
+            double guess = Double.parseDouble(JOptionPane.showInputDialog("What's your guess?")); //getting input
+            if (guess == num){ //if your guess is correct
+                JOptionPane.showMessageDialog(null, "You got it right! The number was "+num); //display answer
+                start = false; //end loop
+            } 
+            else if (guess > num){ //if your guess was too high, tell the user and loop
+                JOptionPane.showMessageDialog(null, "Guess lower!");
+            }
+            else if (guess < num){ //if your guess was too low, tell the user and loop
+                JOptionPane.showMessageDialog(null, "Guess higher!");
+            }
+            }
+        }
+    }
